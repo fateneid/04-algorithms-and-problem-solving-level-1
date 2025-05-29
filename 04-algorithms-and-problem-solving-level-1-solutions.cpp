55464b46ddaec51454731f0a25d0366efe2b48e1
Faten Eid <mulan.tech.914h@gmail.com>
2025-05-29 03:15:50 +0300

    remove unnecessary lines and comments

--- 04-algorithms-and-problem-solving-level-1-solutions.cpp
+++ 04-algorithms-and-problem-solving-level-1-solutions.cpp
@@ -1,71 +1,13 @@
 #include <iostream>
 #include "P01_PrintYourName.h"
-#include "P02_PrintNameFromUser.h"
-#include "P03_OddOrEven.h"
-#include "P04_HireADriverCase1.h"
-#include "P05_HireADriverCase2.h"
-#include "P06_FullName.h"
-#include "P07_HalfNumber.h"
-#include "P08_PassOrFail.h"
-#include "P09_SumOf3Numbers.h"
-#include "P10_AverageOf3Marks.h"
-#include "P11_AveragePassFail.h"
-#include "P12_MaxOf2Numbers.h"
-#include "P13_MaxOf3Numbers.h"
-#include "P14_SwapNumbers.h"
-#include "P15_RectangleArea.h"
-#include "P16_RectangleAreaThroughDiagonalAndSideArea.h"
-#include "P17_TriangleArea.h"
-#include "P18_CircleArea.h"
-#include "P19_CircleAreaThroughDiameter.h"
-#include "P20_CircleAreaInscribedInASquare.h"
-#include "P21_CircleAreaAlongTheCircumference.h"
-#include "P22_CircleAreaInscribedInAnIsoscelesTriangle.h"
-#include "P23_CircleAreaCircleDescribedAroundAnArbitraryTriangle.h"
-#include "P24_ValidateAgeBetween18And45.h"
-#include "P25_ReadUntilAgeBetween18And45.h"
-#include "P26_PrintNumbersFrom1ToN.h"
-#include "P27_PrintNumbersFromNTo1.h"
-#include "P28_PrintSumOddNumbersFrom1ToN.h"
-#include "P29_PrintSumEvenNumbersFrom1ToN.h"
-#include "P30_FactorialOfN.h"
-#include "P31_PowerOf2_3_4.h"
-#include "P32_PowerOfM.h"
-#include "P33_GradeA_B_C_D_E_F.h"
-#include "P34_CommissionPercentage.h"
-#include "P35_PiggyBankCalculator.h"
-#include "P36_SimpleCalculator.h"
-#include "P37_SumUntilMinus99.h"
-#include "P38_IsPrimeNumber.h"
-#include "P39_PayRemainder.h"
-#include "P40_ServiceFeeAndSalesTax.h"
-#include "P41_WeeksAndDays.h"
-#include "P42_TaskDurationInSeconds.h"
-#include "P43_SecondsToDaysHoursMinutesSeconds.h"
-#include "P44_DayOfWeek.h"
-#include "P45_MonthOfYear.h"
-#include "P46_PrintLettersFromAToZ.h"
-#include "P47_LoanInstalmentMonths.h"
-#include "P48_MonthlyLoanInstallment.h"
-#include "P49_ATMPIN.h"
-#include "P50_ATMPIN3Times.h"
-
 
 using namespace std;
 
 int main()
 {
+
+
     P01_PrintYourName::Task();
+
+
 }
-
-/*
-
-library's namespace name = library name
-
-To run any problem:
-
-    - include its library
-    - use its namespace with any method
-    - run Task fun under its namespace
-
-*/
